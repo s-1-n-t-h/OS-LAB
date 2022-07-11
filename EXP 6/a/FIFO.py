fe1814244eeef202c14a1fea32f092b02c061036
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2022-07-11 15:26:48 +0530

    fixed bug

--- EXP 6/a/FIFO.py
+++ EXP 6/a/FIFO.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 #reference string
 rString = list(map(int,input("\nReference String: ").split(" ")))
 fSize = int(input("\nFrame Size: ")) #frames size of ram
@@ -5,19 +6,30 @@ fSize = int(input("\nFrame Size: ")) #frames size of ram
 def FIFO(rString,fSize):
     ram = [] #storing frames in ram
     count = 0 #counting page faults
+    dict = {}
     for i in range(len(rString)):
         #implementing the algorithm
         if rString[i] in ram:
             continue
         elif len(ram) < fSize:
+            dict[rString[i]] = datetime.now()
             ram.append(rString[i])
             count += 1
         else:
-            ram.pop(0)
-            ram.insert(0,rString[i])
-            count += 1
+            leastUsed = sorted(dict.items(), key=lambda x: x[1])
+            for j in leastUsed:
+                pg_no = j[0]
+                if pg_no in ram:
+                    index = ram.index(pg_no)
+                    ram.pop(index)
+                    dict[rString[i]] = datetime.now()
+                    ram.insert(index,rString[i])
+                    count += 1
+                    break
     return count
 
+
+    
 print("\nFIFO: ",FIFO(rString,fSize))
         
 
